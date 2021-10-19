8267ca0b5654a76b422975216fde78ebea55ee8c
Farhan Angullia <angullia@amazon.com>
2021-10-19 17:46:35 +0800

    added mock test for aurora

--- tests/test_rds_actions.py
+++ tests/test_rds_actions.py
@@ -10,24 +10,62 @@ def test_fail_az_rds_basic(client):
     az = "ap-southeast-1a"
     dry_run = False
     db_instance_identifier = "mysqlinstance"
-    multi_az = True
-    tags = [{"Key": "AZ_FAILURE", "Value": "True"}]
+    db_cluster_identifier = "my-cluster"
 
-    mock_client.get_paginator.return_value.paginate.return_value = [
-        {
-            "DBInstances": [
-                {
-                    "DBInstanceIdentifier": db_instance_identifier,
-                    "AvailabilityZone": az,
-                    "MultiAZ": multi_az,
-                    "TagList": tags,
-                },
-            ]
-        }
-    ]
+    mock_client.get_paginator = get_mock_paginate
 
     fail_az(az=az, dry_run=dry_run)
 
     mock_client.reboot_db_instance.assert_called_with(
         DBInstanceIdentifier=db_instance_identifier, ForceFailover=True
     )
+
+    mock_client.failover_db_cluster.assert_called_with(
+        DBClusterIdentifier=db_cluster_identifier
+    )
+
+
+def get_mock_paginate(operation_name):
+    return {
+        "describe_db_instances": MagicMock(
+            paginate=MagicMock(
+                return_value=[
+                    {
+                        "DBInstances": [
+                            {
+                                "DBInstanceIdentifier": "mysqlinstance",
+                                "AvailabilityZone": "ap-southeast-1a",
+                                "MultiAZ": True,
+                                "TagList": [{"Key": "AZ_FAILURE", "Value": "True"}],
+                            },
+                        ]
+                    }
+                ]
+            )
+        ),
+        "describe_db_clusters": MagicMock(
+            paginate=MagicMock(
+                return_value=[
+                    {
+                        "DBClusters": [
+                            {
+                                "DBClusterIdentifier": "my-cluster",
+                                "MultiAZ": True,
+                                "DBClusterMembers": [
+                                    {
+                                        "DBInstanceIdentifier": "my-post-gres-instance-1",
+                                        "IsClusterWriter": True,
+                                    },
+                                    {
+                                        "DBInstanceIdentifier": "my-post-gres-instance-2",
+                                        "IsClusterWriter": False,
+                                    },
+                                ],
+                                "TagList": [{"Key": "AZ_FAILURE", "Value": "True"}],
+                            },
+                        ]
+                    }
+                ]
+            )
+        ),
+    }.get(operation_name, MagicMock())
